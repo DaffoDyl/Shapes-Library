39fe94c029918adb96f78c257f555f7c637ab08c
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 07:45:33 -0600

    Add Line class

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,2 +1,18 @@
-class Line {
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class Line(
+    private var point1 : Point,
+    private var point2 : Point
+): Shape {
+    override fun move(deltaX: Double, deltaY: Double) {
+        point1.move(deltaX, deltaY)
+        point2.move(deltaX, deltaY)
+    }
+    fun getSlope() : Double {
+        return (point2.getY() - point1.getY())/(point2.getX() - point1.getX())
+    }
+    fun getLength() : Double {
+        return sqrt((point2.getX() - point1.getX()).pow(2) + (point2.getY() - point1.getY()).pow(2))
+    }
 }
