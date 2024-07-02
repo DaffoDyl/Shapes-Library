81969ae2c0ad38df0b1914a8d749fa6918c42036
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 08:12:21 -0600

    Add ellipse class

--- src/main/kotlin/Ellipse.kt
+++ src/main/kotlin/Ellipse.kt
@@ -1,2 +1,17 @@
-class Ellipse {
+class Ellipse(
+    private var center : Point,
+    private var radius1 : Double,
+    private var radius2 : Double
+
+): Shape {
+    override fun move(deltaX: Double, deltaY: Double) {
+        center.move(deltaX, deltaY)
+    }
+    fun getCenter(): Point { return center.copy() }
+    fun getRadius1(): Double { return radius1 }
+    fun getRadius2(): Double { return radius2 }
+    fun getArea(center: Point, radius1: Double, radius2: Double): Double {
+        //TODO
+        return 0.0
+    }
 }
