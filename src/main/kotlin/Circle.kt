1f93e851eccb88d24706a6b91ee1ebb9ac44b20c
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 08:10:47 -0600

    Add circle class

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,2 +1,14 @@
-class Circle {
+class Circle(
+    private var center : Point,
+    private var radius : Double
+): Shape {
+    override fun move(deltaX: Double, deltaY: Double) {
+        center.move(deltaX, deltaY)
+    }
+    fun getCenter(): Point { return center.copy() }
+    fun getRadius(): Double { return radius }
+    fun getArea(center: Point, radius: Double): Double {
+        //TODO
+        return 0.0
+    }
 }
