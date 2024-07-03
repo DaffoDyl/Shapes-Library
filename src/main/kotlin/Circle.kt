1a0e12acd0a10f2fc7b50f08799e349fac826126
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 21:06:15 -0600

    Adjust how circle is defined, add more test

--- src/main/kotlin/Circle.kt
+++ src/main/kotlin/Circle.kt
@@ -1,4 +1,4 @@
 class Circle(
-    private var center : Point,
-    private var radius1 : Double
+    center : Point,
+    radius1 : Double
 ): Ellipse(center, radius1) {}
--- src/test/kotlin/CircleTest.kt
+++ src/test/kotlin/CircleTest.kt
@@ -12,6 +12,13 @@ class CircleTest {
         assertEquals(10.0, circle.getRadius2())
     }
     @Test
+    fun testMove() {
+        val circle = Circle(Point(0.0,5.0), 10.0)
+        circle.move(10.0, -10.0)
+        assertEquals(10.0, circle.getCenter().getX())
+        assertEquals(-5.0, circle.getCenter().getY())
+    }
+    @Test
     fun testGetArea() {
         val circle = Circle(Point(0.0,5.0), 10.0)
         assertEquals(PI * 100, circle.getArea())
