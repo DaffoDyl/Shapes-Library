7ef79523f4f3e7d00418e178c5bcec117df6b142
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 22:03:17 -0600

    Add square tests

--- src/test/kotlin/SquareTest.kt
+++ src/test/kotlin/SquareTest.kt
@@ -1,6 +1,25 @@
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class SquareTest {
     @Test
-    fun testSquareConstruction() {}
+    fun testSquareConstruction() {
+        val square = Square(Point(0.0, 5.0), 10.0)
+        assertEquals(0.0, square.getLowestPoint().getX())
+        assertEquals(5.0, square.getLowestPoint().getY())
+        assertEquals(10.0, square.getXLength())
+        assertEquals(10.0, square.getYLength())
+    }
+    @Test
+    fun testMove() {
+        val square = Square(Point(0.0, 5.0), 10.0)
+        square.move(10.0,-10.0)
+        assertEquals(10.0, square.getLowestPoint().getX())
+        assertEquals(-5.0, square.getLowestPoint().getY())
+    }
+    @Test
+    fun testGetArea() {
+        val square = Square(Point(0.0, 5.0), 10.0)
+        assertEquals(100.0, square.getArea())
+    }
 }
