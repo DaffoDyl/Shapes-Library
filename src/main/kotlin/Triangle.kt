56b47f12724f22e09f97596af6ab96272595fd8a
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 22:30:22 -0600

    Add triangle tests and functions

--- src/main/kotlin/Triangle.kt
+++ src/main/kotlin/Triangle.kt
@@ -1,2 +1,21 @@
-class Triangle {
+import kotlin.math.abs
+
+class Triangle(
+    private var point1 : Point,
+    private var point2 : Point,
+    private var point3 : Point,
+): Shape, Plain {
+    override fun move(deltaX: Double, deltaY: Double) {
+        point1.move(deltaX, deltaY)
+        point2.move(deltaX, deltaY)
+        point3.move(deltaX, deltaY)
+    }
+    override fun getArea(): Double {
+        return 0.5 * abs((point1.getX() * (point2.getY() - point3.getY()))
+        + (point2.getX() * (point3.getY() - point1.getY()))
+        + (point3.getX() * (point1.getY() - point2.getY())))
+    }
+    fun getPoint1(): Point { return point1.copy() }
+    fun getPoint2(): Point { return point2.copy() }
+    fun getPoint3(): Point { return point3.copy() }
 }
--- src/test/kotlin/TriangleTest.kt
+++ src/test/kotlin/TriangleTest.kt
@@ -1,2 +1,31 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 class TriangleTest {
+    @Test
+    fun testTriangleConstruction() {
+        val triangle = Triangle(Point(0.0, 1.0), Point(3.0, 1.0), Point(3.0, 5.0))
+        assertEquals(0.0, triangle.getPoint1().getX())
+        assertEquals(1.0, triangle.getPoint1().getY())
+        assertEquals(3.0, triangle.getPoint2().getX())
+        assertEquals(1.0, triangle.getPoint2().getY())
+        assertEquals(3.0, triangle.getPoint3().getX())
+        assertEquals(5.0, triangle.getPoint3().getY())
+    }
+    @Test
+    fun testMove() {
+        val triangle = Triangle(Point(0.0, 1.0), Point(3.0, 1.0), Point(3.0, 5.0))
+        triangle.move(10.0, -10.0)
+        assertEquals(10.0, triangle.getPoint1().getX())
+        assertEquals(-9.0, triangle.getPoint1().getY())
+        assertEquals(13.0, triangle.getPoint2().getX())
+        assertEquals(-9.0, triangle.getPoint2().getY())
+        assertEquals(13.0, triangle.getPoint3().getX())
+        assertEquals(-5.0, triangle.getPoint3().getY())
+    }
+    @Test
+    fun testGetArea() {
+        val triangle = Triangle(Point(0.0, 1.0), Point(3.0, 1.0), Point(3.0, 5.0))
+        assertEquals(6.0, triangle.getArea())
+    }
 }
