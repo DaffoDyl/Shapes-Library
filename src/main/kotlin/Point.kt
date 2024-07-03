2410d911ee6af3ed93705260146f31895d94ac8f
Dylan Armstrong <dylan.armstrong299@gmail.com>
2024-07-02 19:13:56 -0600

    Add testing to point

--- src/main/kotlin/Point.kt
+++ src/main/kotlin/Point.kt
@@ -2,9 +2,10 @@ class Point(
     private var x: Double,
     private var y: Double
 ) : Shape {
+
     override fun move(deltaX: Double, deltaY: Double) {
         x+=deltaX
-        y+=deltaX
+        y+=deltaY
     }
     fun getX(): Double { return x }
     fun getY(): Double { return y }
--- src/test/kotlin/PointTest.kt
+++ src/test/kotlin/PointTest.kt
@@ -1,6 +1,26 @@
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class PointTest {
     @Test
-    fun testPointConstruction() {}
+    fun testPointConstruction() {
+        val point = Point(25.0,-10.0)
+        assertEquals(25.0, point.getX())
+        assertEquals(-10.0, point.getY())
+    }
+    @Test
+    fun testMove() {
+        val point = Point(25.0,-10.0)
+        point.move(25.0, -10.0)
+        assertEquals(50.0, point.getX())
+        assertEquals(-20.0, point.getY())
+    }
+    @Test
+    fun testCopy() {
+        val point = Point(25.0,-10.0)
+        val copy = point.copy()
+        assertEquals(25.0, copy.getX())
+        assertEquals(-10.0, copy.getY())
+
+    }
 }
